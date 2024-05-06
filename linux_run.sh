6f486d44a87c9e644f4a9f48fa02bd338cd16f18
joshnsolomon <joshnsolomon@gmail.com>
2024-05-06 15:27:28 -0700

    cleaning

--- linux_run.sh
+++ linux_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-clang -g main.c met.c timer.c widgets.c `sdl2-config --cflags --libs` -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -o linapp  && ./linapp
+gcc main.c met.c timer.c widgets.c `sdl2-config --cflags --libs` -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -o Cetronome  && ./Cetronome
